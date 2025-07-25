74f96fc5e9b25cd968719f15e3315916cd64d1f0
Mirza Hussain <mohammedhussain2727@gmail.com>
2025-07-25 21:02:53 +0530

    fiexed the links

--- src/app/index.mjs
+++ src/app/index.mjs
@@ -24,7 +24,7 @@ app.use(express.json({limit:"100mb"}));
 app.use(express.urlencoded({extended:true, limit:"100mb"}));
 app.use(cors({
     methods:["GET","POST","PATCH","DELETE","PUT","OPTIONS"],
-    origin:["spy-chat.vercel.app","http://localhost:3000","http://localhost:3001","http://localhost:3002"],
+    origin:["https://spy-chat-appmmhb.vercel.app","http://localhost:3000","http://localhost:3001","http://localhost:3002"],
     credentials:true
 }))
 app.use(session({
@@ -49,7 +49,7 @@ const httpServer=http.createServer(app);
 const io=new Server(httpServer,{
     cors:{
     methods:["GET","POST","PATCH","DELETE","PUT","OPTIONS"],
-    origin:["https://spy-chat.vercel.app","http://localhost:3000","http://localhost:3001","http://localhost:3002"],
+    origin:["https://spy-chat-appmmhb.vercel.app","http://localhost:3000","http://localhost:3001","http://localhost:3002"],
     credentials:true
     }
 })
